7a7e1aaa242fda4aefa69712214ec826703eb938
Alessio Cocchieri <alessio.cocchieri@outlook.it>
2024-11-13 18:28:40 +0100

    re-organized scripts

---
+++ scripts/utils.sh
@@ -0,0 +1,4 @@
+python3 -m src.utils.utils \
+    --print_chat_messages \
+    --input_filename out/completions/Qwen2.5-Math-7B-Instruct/completions_tir.jsonl \
+    --model_tokenizer_id Qwen/Qwen2.5-Math-7B-Instruct \
