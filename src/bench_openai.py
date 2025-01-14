6fda82863f49369613ad5579d3fe9d55cbf96b5a
Alessio Cocchieri <alessio.cocchieri@outlook.it>
2025-01-14 15:37:29 +0100

    add support for deepseek-V3 API

--- src/bench_openai.py
+++ src/bench_openai.py
@@ -17,15 +17,15 @@ from typing import Optional
 from dataclasses import dataclass, field
 from collections import Counter, defaultdict 
 
-#codellama/CodeLlama-34b-Instruct-hf #bigcode/starcoder2-15b-instruct-v0.1 #mistralai/Codestral-22B-v0.1 #deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct #TheBloke/CodeLlama-70B-Instruct-AWQ #casperhansen/llama-3-70b-instruct-awq #hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4
+#deepseek-chat #gpt-4o-2024-08-06
 
 @dataclass
 class ScriptArguments:
-    model_name: Optional[str] = field(default="gpt-4o-2024-08-06", metadata={"help": "model's HF directory or local path"})
-    dataset_name: Optional[str] = field(default="alecocc/mathematic_games_dataset_en")
+    model_name: Optional[str] = field(default="deepseek-chat", metadata={"help": "model's HF directory or local path"})
+    dataset_name: Optional[str] = field(default="alecocc/mathematic_games_dataset_en_2024_def")
     out_dir: Optional[str] =  field(default="./out", metadata={"help": "outputs directory"})
     max_samples: Optional[int] = field(default=-1, metadata={"help": "Maximum number of data to process in train set. Default is -1 to process all data."})
-    start_idx: Optional[int] = field(default=0, metadata={"help": "Index of first prompt to process."})
+    start_idx: Optional[int] = field(default=4, metadata={"help": "Index of first prompt to process."})
     top_p: Optional[float] = field(default=1.0, metadata={"help": "Top p sampling."})
     n_out_sequences: Optional[int] = field(default=1, metadata={"help": "Number of generated sequences per instance"})
     temperature: Optional[float] = field(default=0.0, metadata={"help": "Sampling temperature parameter"})
@@ -70,9 +70,7 @@ if __name__ == "__main__":
     load_dotenv()
 
     OPENAI_KEY = os.getenv("OPENAI_KEY")
-    client = OpenAI(
-        api_key=OPENAI_KEY
-    )
+    DEEPSEEK_KEY = os.getenv("DEEPSEEK_API_KEY")
 
     HF_TOKEN = os.getenv("HF_TOKEN")
     login(token=HF_TOKEN)
@@ -90,7 +88,18 @@ if __name__ == "__main__":
     # parse input args
     parser = HfArgumentParser(ScriptArguments)
     args = parser.parse_args_into_dataclasses()[0]
-    MODEL_NAME =  args.model_name 
+
+    if "deepseek" in args.model_name.lower():
+        client = OpenAI(
+            api_key=DEEPSEEK_KEY,
+            base_url="https://api.deepseek.com"
+        )
+    else:
+        client = OpenAI(
+            api_key=OPENAI_KEY
+        )
+
+    MODEL_NAME = "deepseek-chat" if "deepseek" in args.model_name.lower() else args.model_name 
 
     dataset = load_dataset(args.dataset_name, split="train")
     if args.text_only: # to use to ignore images from data
